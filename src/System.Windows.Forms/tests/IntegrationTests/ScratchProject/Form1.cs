41ce02505c3ff703ce5f91b7f3b75d5aeceade9c
Tanya Solyanik <tanyaso@microsoft.com>
2024-05-01 15:09:40 -0700

    https://github.com/dotnet/winforms/issues/11243

--- src/System.Windows.Forms/tests/IntegrationTests/ScratchProject/Form1.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/ScratchProject/Form1.cs
@@ -9,8 +9,46 @@ namespace ScratchProject;
 [DesignerCategory("Default")]
 public partial class Form1 : Form
 {
+    private readonly TreeView _treeView1;
+
     public Form1()
     {
         InitializeComponent();
+        _treeView1 = new()
+        {
+            Size = new Size(80, 200),
+            Location = new Point(10,10)
+        };
+
+        Controls.Add(_treeView1);
+
+        _treeView1.BeginUpdate();
+        _treeView1.Nodes.Add(new TreeNode("root"));
+        _treeView1.Nodes[0].Nodes.Add(new TreeNode("child"));
+        _treeView1.Nodes[0].Expand();
+        _treeView1.EndUpdate();
+
+        Repro();
+    }
+
+    private void Repro()
+    {
+        Load += (s, e) =>
+        {
+            NoRepro();
+        };
+    }
+
+    private void NoRepro()
+    {
+        TreeNode a0 = new("a0");
+        TreeNode b0 = new("b0");
+        TreeNode c0 = new("c0");
+        _treeView1.Nodes[0].Nodes.AddRange([a0, b0, c0]);
+
+        TreeNode a1 = new("a1");
+        TreeNode b1 = new("b1");
+        TreeNode c1 = new("c1");
+        _treeView1.Nodes[0].Nodes.AddRange([a1, b1, c1]);
     }
 }
