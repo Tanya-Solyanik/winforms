d0f408bcf50e524169ae9ed044fc4a262b2d76a4
Tanya Solyanik <tanyaso@microsoft.com>
2024-06-22 00:14:58 -0700

    my tests

--- src/System.Windows.Forms/tests/IntegrationTests/ScratchProject/Form1.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/ScratchProject/Form1.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.ComponentModel;
+using System.Reflection;
+using System.Reflection.Metadata;
 
 namespace ScratchProject;
 
@@ -12,5 +14,186 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+
+        Action[] writers =
+        {
+            ClipboardSetData
+        };
+
+        Action[] readers =
+        {
+            ClipboardGetData
+        };
+
+        int x = 40;
+        int y = 10;
+        Button button;
+        foreach (var write in writers)
+        {
+            button = new()
+            {
+                Text = write.GetMethodInfo().Name,
+                Location = new Point(x, y),
+                Size = new Size(160, 50)
+            };
+
+            button.Click += (sender, e) => write();
+            Controls.Add(button);
+
+            y += 60;
+        }
+
+        x += 200;
+        y = 10;
+        foreach (var read in readers)
+        {
+            button = new()
+            {
+                Text = read.GetMethodInfo().Name,
+                Location = new Point(x, y),
+                Size = new Size(160, 50)
+            };
+
+            button.Click += (sender, e) => read();
+            Controls.Add(button);
+
+            y += 60;
+        }
+
+        Action[] roundtrips =
+        {
+            RoundTripCustomFormats,
+            RoundTripPenData
+        };
+
+        x += 200;
+        y = 10;
+        foreach (var roundtrip in roundtrips)
+        {
+            button = new()
+            {
+                Text = roundtrip.GetMethodInfo().Name,
+                Location = new Point(x, y),
+                Size = new Size(160, 50)
+            };
+
+            button.Click += (sender, e) => roundtrip();
+            Controls.Add(button);
+
+            y += 60;
+        }
     }
+
+    private void ClipboardSetData()
+    {
+        // DayOfWeek data = DayOfWeek.Friday;
+        // Clipboard.SetData(DataFormats.Serializable, data);
+        // Text = typeof(DayOfWeek).AssemblyQualifiedName;
+
+        // int[] data = { 1, 2, 3, 4, 5 };
+        // Clipboard.SetData("Test format", data);
+        // Text = "int[]";
+
+        // Clipboard.SetData("Test format", new BinaryFormatReadableData());
+        // Text = "BinaryFormatReadableData";
+
+        // Clipboard.SetData(DataFormats.Text, "Hello World");
+        // Text = "Hello world!";
+
+        // Clipboard.SetData(
+        //    DataFormats.Bitmap, new Bitmap("D:\\winforms\\src\\System.Windows.Forms\\tests\\UnitTests\\bitmaps\\nature24bits.jpg"));
+        // Text = typeof(Bitmap).AssemblyQualifiedName;
+
+        Clipboard.SetData(DataFormats.Serializable, Color.AliceBlue);
+        Text = typeof(Color).AssemblyQualifiedName;
+    }
+
+    private void ClipboardGetData()
+    {
+        // object? data = Clipboard.GetData(DataFormats.Bitmap);
+        // if (data is not null)
+        // if (Clipboard.TryGetData<Bitmap>(DataFormats.Bitmap, out Bitmap? data))
+        // if (Clipboard.TryGetData<string>(DataFormats.Text, out string? data))
+        // if (Clipboard.TryGetData<BinaryFormatReadableData>("Test format", out BinaryFormatReadableData? data))
+        // if (Clipboard.TryGetData("Test format", out int[]? data))
+        // if (Clipboard.TryGetData(DataFormats.Serializable, out DayOfWeek? data))
+        if (Clipboard.TryGetData(DataFormats.Serializable, out Color data))
+        {
+            Text = $"got {data.GetType().AssemblyQualifiedName}";
+        }
+        else
+        {
+            Text = "Could not retrieve data off the clipboard.";
+        }
+
+        Clipboard.Clear();
+    }
+
+    // Get the bitmap from clipboard in another application with targeting 4.8
+
+    #region RoundTrips
+    public void RoundTripPenData()
+    {
+        Font f1 = new("Microsoft Sans Serif", 10);
+        Clipboard.SetData(DataFormats.PenData, f1);
+        object? o = Clipboard.GetData(DataFormats.PenData);
+        if (o is Font f)
+        {
+            Text = f.Name;
+        }
+        else
+        {
+            Text = "null";
+        }
+    }
+
+    private static Type WaveAudioResolver(TypeName typeName)
+    {
+        Type[] knownTypes = [typeof(MemoryStream), typeof(Stream)];
+
+        foreach (Type type in knownTypes)
+        {
+            TypeName parsed = TypeName.Parse($"{type.FullName}, {type.Assembly.FullName}");
+
+            if (typeName.FullName == parsed.FullName && typeName.AssemblyName?.Name == parsed.AssemblyName?.Name)
+            {
+                return type;
+            }
+        }
+
+        throw new NotSupportedException();
+    }
+
+    public void RoundTripCustomFormats()
+    {
+        byte[] audioBytes = [1, 2, 3];
+        Clipboard.SetAudio(audioBytes);
+
+        bool result = Clipboard.TryGetData(DataFormats.WaveAudio, WaveAudioResolver, out Stream? unboundedData);
+        Text = result.ToString();
+
+        // string myTextFormat = "MyTextFormat";
+        // string myBlobFormat = "MyBlobFormat";
+        // Guid guid = Guid.NewGuid();
+        // string myText = guid.ToString();
+        // MemoryStream myBlob = new(guid.ToByteArray());
+        // string unicodeText = "Euro char: \u20AC";
+        // DataObject data = new();
+        // data.SetData(myTextFormat, myText);
+        // data.SetData(myBlobFormat, myBlob);
+        // data.SetText(unicodeText);
+        // Clipboard.SetDataObject(data);
+        // IDataObject? copiedDataObject = Clipboard.GetDataObject();
+        // string? copiedText = copiedDataObject?.GetData(myTextFormat) as string;
+        // MemoryStream? copiedBlob = copiedDataObject?.GetData(myBlobFormat) as MemoryStream;
+        // string? copiedUnicodeText = Clipboard.GetText();
+        // Assert.NotNull(copiedText);
+        // Assert.Equal(copiedText, myText);
+        // Assert.NotNull(copiedBlob);
+        // Assert.Equal(copiedBlob.Length, myBlob.Length);
+        // Assert.Equal(new Guid(copiedBlob.ToArray()), guid);
+        // Assert.NotNull(copiedUnicodeText);
+        // Assert.Equal(copiedUnicodeText, unicodeText);
+    }
+    #endregion
 }
