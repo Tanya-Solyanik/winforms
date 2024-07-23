ab3d771f8c02534f97b8e4ac4a9971e002741d82
Tanya Solyanik <tanyaso@microsoft.com>
2024-07-22 18:19:45 -0700

    refactored for testing

--- src/System.Windows.Forms/src/System/Windows/Forms/OLE/DataObject.Composition.NativeToWinFormsAdapter.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/OLE/DataObject.Composition.NativeToWinFormsAdapter.cs
@@ -1,15 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Runtime.Serialization.Formatters;
 using System.Text;
-using System.Private.Windows.Core.BinaryFormat;
 using Com = Windows.Win32.System.Com;
-using System.Drawing;
-using System.Windows.Forms.BinaryFormat;
 
 namespace System.Windows.Forms;
 
@@ -176,43 +172,7 @@ public unsafe partial class DataObject
                 static object ReadObjectFromHGLOBAL(HGLOBAL hglobal, bool restrictDeserialization)
                 {
                     MemoryStream stream = ReadByteStreamFromHGLOBAL(hglobal, out bool isSerializedObject);
-                    return !isSerializedObject ? stream : ReadObjectFromHandleDeserializer(stream, restrictDeserialization);
-
-                    static object ReadObjectFromHandleDeserializer(Stream stream, bool restrictDeserialization)
-                    {
-                        long startPosition = stream.Position;
-                        try
-                        {
-                            if (new BinaryFormattedObject(stream).TryGetObject(out object? value))
-                            {
-                                return value;
-                            }
-                        }
-                        catch (Exception ex) when (!ex.IsCriticalException())
-                        {
-                            // Couldn't parse for some reason, let the BinaryFormatter try to handle it.
-                        }
-
-                        // This check is to help in trimming scenarios with a trim warning on a call to BinaryFormatter.Deserialize(), which has a RequiresUnreferencedCode annotation.
-                        // If the flag is false, the trimmer will not generate a warning, since BinaryFormatter.Deserialize() will not be called,
-                        // If the flag is true, the trimmer will generate a warning for calling a method that has a RequiresUnreferencedCode annotation.
-                        if (!EnableUnsafeBinaryFormatterInNativeObjectSerialization)
-                        {
-                            throw new NotSupportedException(SR.BinaryFormatterNotSupported);
-                        }
-
-                        stream.Position = startPosition;
-
-#pragma warning disable SYSLIB0011 // Type or member is obsolete
-#pragma warning disable SYSLIB0050 // Type or member is obsolete
-                        return new BinaryFormatter()
-                        {
-                            Binder = restrictDeserialization ? new BitmapBinder() : null,
-                            AssemblyFormat = FormatterAssemblyStyle.Simple
-                        }.Deserialize(stream);
-#pragma warning restore SYSLIB0050
-#pragma warning restore SYSLIB0011
-                    }
+                    return !isSerializedObject ? stream : BinaryFormatUtilities.ReadObjectFromStream(stream, restrictDeserialization);
 
                     static unsafe MemoryStream ReadByteStreamFromHGLOBAL(HGLOBAL hglobal, out bool isSerializedObject)
                     {
