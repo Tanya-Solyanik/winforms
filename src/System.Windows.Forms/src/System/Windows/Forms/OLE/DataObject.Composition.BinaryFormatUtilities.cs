e54e30af5cbee015f9fa356618be6a1106768a1e
Tanya Solyanik <tanyaso@microsoft.com>
2024-11-06 16:26:12 -0800

    Adding TryGetData<T> API

--- src/System.Windows.Forms/src/System/Windows/Forms/OLE/DataObject.Composition.BinaryFormatUtilities.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/OLE/DataObject.Composition.BinaryFormatUtilities.cs
@@ -1,10 +1,15 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Formats.Nrbf;
+using System.Private.Windows.Core.BinaryFormat;
+using System.Reflection.Metadata;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms.BinaryFormat;
 using System.Windows.Forms.Nrbf;
+using System.Windows.Forms.Primitives;
 
 namespace System.Windows.Forms;
 
@@ -29,42 +34,123 @@ public unsafe partial class DataObject
                     Debug.Fail($"Unexpected exception writing binary formatted data. {ex.Message}");
                 }
 
-#pragma warning disable SYSLIB0011 // Type or member is obsolete
                 if (!success)
                 {
-                    // This check is to help in trimming scenarios with a trim warning on a call to BinaryFormatter.Serialize(),
-                    // which has a RequiresUnreferencedCode annotation.
-                    // If the flag is false, the trimmer will not generate a warning, since BinaryFormatter.Serialize(), will not be called,
-                    // If the flag is true, the trimmer will generate a warning for calling a method that has a RequiresUnreferencedCode annotation.
+                    // This check is to help in trimming scenarios with a trim warning on a call to
+                    // BinaryFormatter.Serialize(), which has a RequiresUnreferencedCode annotation.
+                    // If the flag is false, the trimmer will not generate a warning, since BinaryFormatter.Serialize(),
+                    // will not be called,
+                    // If the flag is true, the trimmer will generate a warning for calling a method that has a
+                    // RequiresUnreferencedCode annotation.
                     if (!EnableUnsafeBinaryFormatterInNativeObjectSerialization)
                     {
                         throw new NotSupportedException(SR.BinaryFormatterNotSupported);
                     }
 
+                    if (!LocalAppContextSwitches.ClipboardDragDropEnableUnsafeBinaryFormatterSerialization)
+                    {
+                        throw new NotSupportedException($"Using BinaryFormatter is not supported in WinForms Clipboard or drag and drop scenarios");
+                    }
+
                     stream.Position = position;
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
                     new BinaryFormatter()
                     {
                         Binder = restrictSerialization ? new BitmapBinder() : null
                     }.Serialize(stream, data);
-                }
 #pragma warning restore SYSLIB0011
+                }
             }
 
-            internal static object ReadObjectFromStream(MemoryStream stream, bool restrictDeserialization)
+            internal static object? ReadObjectFromStream<T>(
+                MemoryStream stream,
+                Func<TypeName, Type>? resolver,
+                bool restrictDeserialization,
+                bool legacyMode)
             {
                 long startPosition = stream.Position;
+                SerializationRecord? record;
+
+                SerializationBinder binder = restrictDeserialization
+                    ? new BitmapBinder()
+                    : new DataObject.Composition.Binder(typeof(T), resolver, legacyMode);
+
+                IReadOnlyDictionary<SerializationRecordId, SerializationRecord> recordMap;
                 try
                 {
-                    if (stream.Decode().TryGetCommonObject(out object? value))
-                    {
-                        return value;
-                    }
+                    record = stream.Decode(out recordMap);
                 }
                 catch (Exception ex) when (!ex.IsCriticalException())
                 {
-                    // Couldn't parse for some reason, let the BinaryFormatter try to handle it.
+                    // Couldn't parse for some reason, let the BinaryFormatter try to handle the legacy invocation.
+                    if (legacyMode && LocalAppContextSwitches.ClipboardDragDropEnableUnsafeBinaryFormatterSerialization)
+                    {
+                        stream.Position = startPosition;
+                        return ReadObjectWithBinaryFormatter<T>(stream, binder);
+                    }
+
+                    // For example offset arrays throw from the decoder -
+                    // https://learn.microsoft.com/dotnet/api/system.array.createinstance?#system-array-createinstance(system-type-system-int32()-system-int32())
+                    throw new NotSupportedException("Clipboard content can't be validated.", ex);
                 }
 
+                // For the new TryGet APIs, ensure that the stream contains the requested type,
+                // or type that can be assigned to the requested type.
+                if (!legacyMode && !record.TypeNameMatches<T>())
+                {
+#if false // TODO(TanyaSo) - modify TryGetObjectFromJson to take a resolver and rename to HasJsonData???
+                    // Return true if the payload contains valid JsonData struct, type matches or not
+                    // note: binder.GetType() throws and never returns null
+                    // run isassignable in the json method
+                    if (record.TryGetObjectFromJson(binder.GetType, out object? data))
+                    {
+                        return data;
+                    }
+#endif
+
+                    if (resolver is null || !TypeNameIsAssignableToType(record.TypeName, typeof(T), resolver))
+                    {
+                        return null;
+                    }
+                }
+
+                if (record.TryGetCommonObject(out object? value))
+                {
+                    return value;
+                }
+
+                if (LocalAppContextSwitches.ClipboardDragDropEnableUnsafeBinaryFormatterSerialization)
+                {
+                    if (!legacyMode && record.Deserialize(recordMap, (ITypeResolver)binder) is { } result)
+                    {
+                       return result;
+                    }
+
+                    stream.Position = startPosition;
+                    return ReadObjectWithBinaryFormatter<T>(stream, binder);
+                }
+
+                return null;
+            }
+
+            // TanyaSo: this does not special-case the NotSupported exception, but we probably want to always deserialize it.
+            private static bool TypeNameIsAssignableToType(TypeName typeName, Type type, Func<TypeName, Type> resolver)
+            {
+                Type? resolvedType = null;
+                try
+                {
+                    resolvedType = resolver(typeName);
+                }
+                catch (Exception ex) when (!ex.IsCriticalException())
+                {
+                    return false;
+                }
+
+                return resolvedType?.IsAssignableTo(type) == true;
+            }
+
+            private static object? ReadObjectWithBinaryFormatter<T>(MemoryStream stream, SerializationBinder binder)
+            {
                 // This check is to help in trimming scenarios with a trim warning on a call to BinaryFormatter.Deserialize(),
                 // which has a RequiresUnreferencedCode annotation.
                 // If the flag is false, the trimmer will not generate a warning, since BinaryFormatter.Deserialize() will not be called,
@@ -83,7 +169,7 @@ public unsafe partial class DataObject
                 // cs/dangerous-binary-deserialization
                 return new BinaryFormatter()
                 {
-                    Binder = restrictDeserialization ? new BitmapBinder() : null,
+                    Binder = binder,
                     AssemblyFormat = FormatterAssemblyStyle.Simple
                 }.Deserialize(stream); // CodeQL[SM03722] : BinaryFormatter is intended to be used as a fallback for unsupported types. Users must explicitly opt into this behavior.
 #pragma warning restore CA2300
