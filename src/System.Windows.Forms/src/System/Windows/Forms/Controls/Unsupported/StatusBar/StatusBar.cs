b100f68101245671428663ba4ce0e133e612f1be
Tanya Solyanik <tanyaso@microsoft.com>
2025-01-15 20:59:35 -0800

    Added top level xml-doc comments

--- src/System.Windows.Forms/src/System/Windows/Forms/Controls/Unsupported/StatusBar/StatusBar.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Controls/Unsupported/StatusBar/StatusBar.cs
@@ -20,6 +20,9 @@ namespace System.Windows.Forms;
 [DefaultEvent(nameof(PanelClick))]
 [DefaultProperty(nameof(Text))]
 [Designer($"System.Windows.Forms.Design.StatusBarDesigner, {AssemblyRef.SystemDesign}")]
+/// <summary>
+///  This type is provided for binary compatibility with the .NET Framework. You should not use it.
+/// </summary>
 public partial class StatusBar : Control
 {
     public StatusBar() => throw new PlatformNotSupportedException();
