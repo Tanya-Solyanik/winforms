ac00f57b2ca820adf29f2b25c748695aa8403833
Tanya Solyanik <tanyaso@microsoft.com>
2025-02-13 10:14:31 -0800

    open dialog test

--- src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/OpenFileDialogTests.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/OpenFileDialogTests.cs
@@ -14,7 +14,7 @@ public class OpenFileDialogTests : ControlTestBase
 
     // Regression test for https://github.com/dotnet/winforms/issues/8108
     [WinFormsFact]
-    public void OpenFileDialogTests_OpenWithNonExistingInitDirectory_Success()
+    public void OpenWithNonExistingInitDirectory_Success()
     {
         using DialogHostForm dialogOwnerForm = new();
         using OpenFileDialog dialog = new();
@@ -23,7 +23,7 @@ public class OpenFileDialogTests : ControlTestBase
     }
 
     [WinFormsFact]
-    public void OpenFileDialogTests_OpenWithExistingInitDirectory_Success()
+    public void OpenWithExistingInitDirectory_Success()
     {
         using DialogHostForm dialogOwnerForm = new();
         using OpenFileDialog dialog = new();
@@ -33,7 +33,7 @@ public class OpenFileDialogTests : ControlTestBase
 
     // Regression test for https://github.com/dotnet/winforms/issues/8414
     [WinFormsFact]
-    public void OpenFileDialogTests_ResultWithMultiselect()
+    public void ShowDialog_ResultWithMultiselect()
     {
         using var tempFile = TempFile.Create(0);
         using AcceptDialogForm dialogOwnerForm = new();
@@ -43,6 +43,53 @@ public class OpenFileDialogTests : ControlTestBase
         dialog.FileName = tempFile.Path;
         Assert.Equal(DialogResult.OK, dialog.ShowDialog(dialogOwnerForm));
         Assert.Equal(tempFile.Path, dialog.FileName);
+    }
+
+    // Regression test for https://github.com/dotnet/winforms/issues/12847
+    [WinFormsFact]
+    public void ShowDialog_Twice_DoesNotCauseStackOverflow()
+    {
+        bool done = false;
+
+        using OpenTwiceHostForm dialogOwnerForm = new(() => done);
+        using OpenFileDialog openFileDialog = new();
+        using ToolStrip toolStrip = new();
+        using ToolStripButton toolStripButton = new()
+        {
+            Text = "Double Click Me!"
+        };
+        toolStrip.Items.Add(toolStripButton);
+        dialogOwnerForm.Controls.Add(toolStrip);
+
+        toolStripButton.Click += (sender, e) =>
+        {
+            openFileDialog.ShowDialog(dialogOwnerForm);
+        };
+
+        // Simulate two clicks on the ToolStripButton to open the OpenFileDialog.
+        toolStripButton.PerformClick();
+        toolStripButton.PerformClick();
+
+        // Verify that the dialog was shown and no stack overflow occurred
+        done = true;
+    }
+
+    private class OpenTwiceHostForm : DialogHostForm
+    {
+        private readonly Func<bool> _done;
+
+        public OpenTwiceHostForm(Func<bool> done)
+        {
+            _done = done;
+        }
+
+        protected override void OnDialogIdle(HWND dialogHandle)
+        {
+            if (_done())
+            {
+                base.OnDialogIdle(dialogHandle);
+            }
+        }
     }
 
     private class AcceptDialogForm : DialogHostForm
