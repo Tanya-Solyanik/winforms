b3559943307f51e53d58e618bcd2ffb2a5b81c78
Tanya Solyanik <tanyaso@microsoft.com>
2025-04-04 15:24:52 -0700

    https://github.com/dotnet/winforms/issues/13199 before the new SDK

--- src/test/integration/ScratchProject/Form1.cs
+++ src/test/integration/ScratchProject/Form1.cs
@@ -12,5 +12,20 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+
+        TextBox textBox = new() { Location = new Point(10, 10) };
+        Controls.Add(textBox);
+        // Create a 256x256 icon for testing
+        using Bitmap bitmap = new(256, 256);
+        using Graphics graphics = Graphics.FromImage(bitmap);
+        graphics.FillRectangle(Brushes.Red, 0, 0, 256, 256);
+        Icon icon = Icon.FromHandle(bitmap.GetHicon());
+
+        ErrorProvider errorProvider = new()
+        {
+            Icon = icon
+        };
+
+        errorProvider.SetError(textBox, "Test error");
     }
 }
